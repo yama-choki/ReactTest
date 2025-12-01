070bf1191212fc62ea40b1b5673e2a89f27be972
D.Yamasaki <lovepingpong0@gmail.com>
2025-12-02 00:11:34 +0900

    describeの使い方について

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,20 @@
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./Greet"
 
-test('挨拶が正常にレンダリングされるか？', () => {
-  render(<Greet />)
-  const textElement = screen.getByText(/hello/i)
-  expect(textElement).toBeInTheDocument() 
-})
 
-test('test with a name', () => {
-  render(<Greet name='Yamachoki'/>)
-  const textElement = screen.getByText(/hello Yamachoki/i)
-  expect(textElement).toBeInTheDocument() 
+
+describe('Greet', () => {
+  test('render greet', () => {
+    render(<Greet />)
+    const textElement = screen.getByText(/hello/i)
+    expect(textElement).toBeInTheDocument() 
+  })
+
+  describe('Nested',() => {
+    test('test with a name', () => {
+      render(<Greet name='Yamachoki'/>)
+      const textElement = screen.getByText(/hello Yamachoki/i)
+      expect(textElement).toBeInTheDocument() 
+    })
+  })
 })
